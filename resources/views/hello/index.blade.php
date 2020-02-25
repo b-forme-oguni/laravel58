cddddd560b8270bd31739459eb72a7f68bafddef
oguni <oguni@b-forme.net>
2020-02-25 11:58:24 +0900

    バリデーション　エラーメッセージのフォーム表示

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -2,20 +2,43 @@
 
 @section('main')
 <p>{{$msg}}</p>
+@if (count($errors) > 0)
+<p>入力に問題があります。再入力して下さい。</p>
+@endif
 <table>
     <form action="/hello" method="post">
-    {{ csrf_field() }}
+        {{ csrf_field() }}
+        @if ($errors->has('name'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('name')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>name: </th>
-            <td><input type="text" name="name"></td>
+            <td><input type="text" name="name" value="{{old('name')}}"></td>
         </tr>
+
+        @if ($errors->has('mail'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('mail')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>mail: </th>
-            <td><input type="text" name="mail"></td>
+            <td><input type="text" name="mail" value="{{old('name')}}"></td>
         </tr>
+
+        @if ($errors->has('age'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('age')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>age: </th>
-            <td><input type="text" name="age"></td>
+            <td><input type="text" name="age" value="{{old('name')}}"></td>
         </tr>
         <tr>
             <th>name: </th>
