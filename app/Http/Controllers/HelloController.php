3eb5ea1083ccfae41d459cf19508894a4c20a855
b-forme-oguni <oguni@b-forme.net>
2020-02-28 14:25:18 +0900

    エラーメッセージを日本語で置き換える

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -40,12 +40,25 @@ class HelloController extends Controller
 
     public function post(Request $request)
     {
-        $validate_rule = [
+        $rules = [
             'name' => 'required',
             'mail' => 'email',
             'age' => 'numeric|between:0,150',
         ];
-        $this->validate($request, $validate_rule);
+
+        $message = [
+            'name.required' => '名前は必ず入力して下さい。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢は整数で記入ください。',
+            'age.between' => '年齢は0～150の間で入力下さい。',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $message);
+        if ($validator->fails()) {
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
         return view('hello.index', ['msg' => '正しく入力されました。！']);
     }
 }
