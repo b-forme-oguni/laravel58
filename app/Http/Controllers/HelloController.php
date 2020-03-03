2dba324b2750ea71c8c1c42248e2486f93581e5c
b-forme-oguni <oguni@b-forme.net>
2020-03-03 15:06:51 +0900

    DB::selectの利用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Book;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\HelloRequest;
 use Illuminate\Support\Facades\Validator;
 
@@ -28,12 +30,8 @@ class HelloController extends Controller
 
     public function index(Request $request)
     {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        return view('hello.index', ['msg' => $msg]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
 
     public function post(Request $request)
