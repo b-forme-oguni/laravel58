37d9adc87bcc932d84b58de1913a1b0ce58002c7
b-forme-oguni <oguni@b-forme.net>
2020-03-05 15:34:55 +0900

    DB::deleteによる削除

--- routes/web.php
+++ routes/web.php
@@ -74,3 +74,7 @@ Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+
+
