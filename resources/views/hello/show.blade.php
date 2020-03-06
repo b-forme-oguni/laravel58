6278dee6251a1c803aca9cdc89bc11999211d3f6
b-forme-oguni <oguni@b-forme.net>
2020-03-06 15:56:34 +0900

    where、orWhereを使った検索抽出

--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -2,19 +2,25 @@
 
 @section('title','Show')
 @section('menuber')
-    @parent
-    詳細ページ
+@parent
+詳細ページ
 @endsection
 
 @section('content')
-    <table>
-    <tr><th>id: </th><td>{{$item->id}}</td></tr>
-    <tr><th>name: </th><td>{{$item->name}}</td></tr>
-    <tr><th>mail: </th><td>{{$item->mail}}</td></tr>
-    <tr><th>age: </th><td>{{$item->age}}</td></tr>
-    </table>
+    @if ($items != null)
+        @foreach ($items as $item)
+            <table width="400px">
+                <tr>
+                    <th width="50px">id: </th>
+                    <td width="50px">{{$item->id}}</td>
+                    <th width="50px">name: </th>
+                    <td>{{$item->name}}</td>
+                </tr>
+            </table>
+        @endforeach
+    @endif
 @endsection
 
 @section('footer')
-    copyright 2017 tuyano.
+copyright 2017 tuyano.
 @endsection
