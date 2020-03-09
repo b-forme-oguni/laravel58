9f52453ac7a63a1395e4c58460ded295a9c9874c
b-forme-oguni <oguni@b-forme.net>
2020-03-09 10:33:36 +0900

    orderByで並び順を指定する

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -31,7 +31,7 @@ class HelloController extends Controller
 
     public function index(Request $request)
     {
-        $items = DB::table('people')->get();
+        $items = DB::table('people')->orderBy('age','asc')->get();
         return view('hello.index', ['items' => $items]);
     }
 
