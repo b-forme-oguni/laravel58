bee2104084fae30204f280a3dd84b45651c22fe5
b-forme-oguni <oguni@b-forme.net>
2020-03-11 16:08:36 +0900

    _ide_helperのインストール

---
+++ _ide_helper_models.php
@@ -0,0 +1,89 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\Article
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Article newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Article newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Article query()
+ */
+	class Article extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Book
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book query()
+ */
+	class Book extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Person
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $mail
+ * @property int $age
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person ageGreaterThan($n)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person ageLessThan($n)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person nameEqual($str)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person whereAge($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person whereMail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Person whereUpdatedAt($value)
+ */
+	class Person extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
