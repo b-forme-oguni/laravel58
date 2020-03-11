d316a82563f2259870bc8d8ccd10ab92c45fe3f8
b-forme-oguni <oguni@b-forme.net>
2020-03-11 13:26:38 +0900

    【Eloquent】IDによる検索

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
+    public function find(Request $request)
+    {
+        return view('person.find', ['input' => '']);
+    }
+
+    public function search(Request $request)
+    {
+        $item = Person::find($request->input);
+        $param = ['input' => $request->input, 'item' => $item];
+        return view('person.find', $param);
+    }
+    
     public function index(Request $request)
     {
         $items = Person::all();
