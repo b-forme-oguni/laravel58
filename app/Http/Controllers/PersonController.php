db50c5c36971dc06c3966fd170fb0a84394dd69f
b-forme-oguni <oguni@b-forme.net>
2020-03-11 14:03:26 +0900

    【Eloquent】whereによる検索

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -14,11 +14,11 @@ class PersonController extends Controller
 
     public function search(Request $request)
     {
-        $item = Person::find($request->input);
+        $item = Person::where('name', $request->input)->first();
         $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
     }
-    
+
     public function index(Request $request)
     {
         $items = Person::all();
