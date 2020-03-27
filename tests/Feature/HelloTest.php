46cb19d48b30b778adf0bd4a7bc6365f9cc8e670
b-forme-oguni <oguni@b-forme.net>
2020-03-27 15:48:41 +0900

    【ユニットテスト】消えてしまったdatabase.sqliteをサルベージ！

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Person;
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -19,19 +20,32 @@ class HelloTest extends TestCase
      */
     public function testHello()
     {
-        $this->assertTrue(true);
+        //ダミーで利用するデーター
+        factory(User::class)->create([
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+        factory(User::class, 10)->create();
 
-        $response = $this->get('/');
-        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', [
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
 
-        $response = $this->get('/hello');
-        $response->assertStatus(302);
+        //ダミーで利用するデーター
+        factory(Person::class, 10)->create([
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
+        factory(Person::class, 10)->create();
 
-        $user = factory(User::class)->create();
-        $response = $this->actingAs($user)->get('/hello');
-        $response->assertStatus(200);
-
-        $response = $this->get('/no_route');
-        $response->assertStatus(404);
+        $this->assertDatabaseHas('people', [
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
     }
 }
