1cd97a819e1d37a86a424e2fa6062c8742317d4b
b-forme-oguni <oguni@b-forme.net>
2020-04-08 11:17:23 +0900

    学習終了

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel_test'),
+            'database' => env('DB_DATABASE', 'laravel58'),
             'username' => env('DB_USERNAME', 'homestead'),
             'password' => env('DB_PASSWORD', '1234'),
             'unix_socket' => env('DB_SOCKET', ''),
